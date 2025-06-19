69b732b3565f029cffb5de4618697ebb5b767bb4
Christian <christian.siegrist@open-circle.ch>
2025-06-19 14:43:21 +0200

    updated script.js, game works but without endresult and explanation

--- script.js
+++ script.js
@@ -3,69 +3,99 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+// 
+// create function to play 5 Rounds track score and declare winner at the end
+    // create function playGame
+    // Set Score Variables for Human and Computer
+    // increase score by 1 for each win
+    // repeat for 5 rounds stop after 5 rounds
+    // call the winner
+
+
+function playGame() {
+    console.log("start")
+    const rounds = 5;
+    let humanScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < rounds; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        let result = playRound(humanChoice, computerChoice);
+        console.log(result);
+        if (result == "youwin") humanScore++;
+        if (result == "youloose") computerScore++;
+        console.log(humanScore, computerScore);
+    }
+
+
+//create function howwin
+//get humanChoice and ComputerChoice
+//call choices and wich choice wins
+
+ //  function howwewin(humanChoice, computerChoice, playround)
+   
+    
+
 // create function getComputerChoice
 // calculate random value
 // determine choice from random value
 // return choice
-function getComputerChoice() { 
-    x = Math.random();
-    if (x < 0.33) return 1;
-    if (x > 0.67) return 2;
-    else return 3;
-}
-console.log(getComputerChoice())
+
+    function getComputerChoice() { 
+        let x = Math.random();
+        if (x < 0.33) return 1;
+        if (x > 0.67) return 2;
+        else return 3;
+    }
 
 // create function getHumanChoice
 // ask for humans choice
+// make parameters case-insensitive
 // get human choice and return it to the function
+// ask again if invalid input
+
+    function getHumanChoice() {
+
+        let Humaninput = prompt("Insert your Choice Rock - Paper - Scissor")
+        let Humaninputlower = Humaninput.toLowerCase()
+        if (Humaninputlower == "rock") return 1;
+        if (Humaninputlower == "paper") return 2;
+        if (Humaninputlower == "scissor") return 3;
+        alert("invalid Input")
+        return getHumanChoice();
+    }
+// create function to play 1 Round
+// define Parameters for humanChoice and computer Choice
+// compare choices and declare winner: 
 
 
-function getHumanChoice() {
+    function playRound(humanChoice, computerChoice) {
+        if (computerChoice === humanChoice) return "draw";
+        if (
+            (humanChoice === 1 && computerChoice === 3) || // rock wins
+            (humanChoice === 2 && computerChoice === 1) || // paper wins
+            (humanChoice === 3 && computerChoice === 2) // scissor wins
+        ) {
+            return "youwin";
+        } 
+        return "youloose";
+        
+    }   
 
-    Humaninput = prompt("Insert your Choice Rock - Paper - Scissor")
-    Humaninputlower = Humaninput.toLowerCase()
-    if (Humaninputlower == "rock") return 1;
-    if (Humaninputlower == "paper") return 2;
-    if (Humaninputlower == "scissor") return 3;
-    alert("invalid Input")
-    return getHumanChoice();
+    
 }
 
 
 
-// Set Score Variables for Human and Computer
 
-
-
-// create function to play 1 Round
-// define Parameters for humanChoice and computer Choice
-// make parameters case-insensitive
-// copare choices and declare winner: 
-    // define Parameters for Choices
-    // redefine string to a Number
-    // compare Numbers to get the Winner
-// increment winnerscore by +1
-
-
-
-// create function to play 5 Rounds track score and declare winner at the end
-    // create function playGame
-    // 
-
-let humanChoice = getHumanChoice()
-let computerChoice = getComputerChoice()
-
-
-// function playGame () {
-    let humanScore = 0;
-    let computerScore = 0;
-
-        function playRound (humanChoice, computerChoice) {
-        return result
-        }
-    
-
-
-
-
-// console.log(humanChoice, computerChoice)
